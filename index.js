2a82e0e797aff800dd8a142a7800d3b8953b9538
djmiless <oawotokun@gmail.com>
2022-04-05 13:55:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const fs = require("fs");
+const { json } = require("express/lib/response");
+
+const PORT = 2000;
+
+server.use(cors());
+server.use(express.json());
+
+
+// routes
+
+server.get("/get-land", function(request, response){
+
+    fs.readFile("db.json", function(error, result){
+        if(error) throw new error;
+
+        result = JSON.parse(result);
+        if(result){
+            response.send({
+                        message: "state retrieved...",
+                        data: result,
+                        code: "success"
+                    })
+        }
+    })
+
+    
+
+
+    
+    // if(request.query.state != null){
+
+    //     let state = request.query.state
+
+    //     // for(let i= 0; i < land.length; i++){
+    //     //     if(land[i].state === state){
+    //     //        land_array = [];
+    //     //        land_array.push(land[i]);
+    //     //        response.send(land_array);
+    //     //        break;
+    //     //     }
+    //     // }
+    // }else{   
+    //     response.send({
+    //         message: "state retrieved...",
+    //         data: [land],
+    //         code: "success"
+    //     })
+    // }
+})
+
+server.listen(PORT, function(){
+    console.log("Running on PORT: ", PORT)
+})
