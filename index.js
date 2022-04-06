c71eb56665433e12ba0ce947aa0d593b7c987897
djmiless <oawotokun@gmail.com>
2022-04-06 12:26:58 +0100

    dotenv file added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const server = express();
 const fs = require("fs");
+require("dotenv").config();
 
 
-const PORT = 2000;
+const PORT = process.env.PORT || 2000;
 
 server.use(cors());
 server.use(express.json());
